02bc5ad23860dee825af60f980fff2f8258362bd
Arch <mint_bin@163.com>
2019-01-28 16:44:43 +0800

    add: 发布0.1.2版本，修复HGCategoryView不能设置内容背景色的bug

--- HGSegmentedPageViewController.podspec
+++ HGSegmentedPageViewController.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'HGSegmentedPageViewController'
-  s.version          = '0.1.1'
+  s.version          = '0.1.2'
   s.license          = 'MIT'
   s.summary          = 'A paging solution'
   s.description      = %{
--- HGSegmentedPageViewController/Classes/HGCategoryView.h
+++ HGSegmentedPageViewController/Classes/HGCategoryView.h
@@ -13,12 +13,13 @@
 @end;
 
 @interface HGCategoryView : UIView
+@property (nonatomic, strong, readonly) UICollectionView *collectionView;
+@property (nonatomic, strong, readonly) UIView *underline;
+@property (nonatomic, strong, readonly) UIView *separator;
 @property (nonatomic, strong) UIFont *titleNomalFont;
 @property (nonatomic, strong) UIFont *titleSelectedFont;
 @property (nonatomic, strong) UIColor *titleNormalColor;
 @property (nonatomic, strong) UIColor *titleSelectedColor;
-@property (nonatomic, strong, readonly) UIView *underline;
-@property (nonatomic, strong, readonly) UIView *separator;
 @property (nonatomic) NSInteger originalIndex;
 @property (nonatomic, readonly) NSInteger selectedIndex;
 @property (nonatomic, copy) NSArray<NSString *> *titles;
